32275f37da844b9ff3e60df762fcf15b196fb38c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2020-06-16 04:16:33 +0200

    Add basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PlanetsListComponent } from './planets-list/planets-list.component';
+import { PlanetDetailsComponent } from './planets-list/planet-details/planet-details.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PlanetsListComponent
+  },
+  {
+    path: 'planets',
+    component: PlanetDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
